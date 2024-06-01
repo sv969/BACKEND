f9b0301fc07131c10073680f5fb34f0a21a30a79
sv969 <sv969@nau.edu>
2024-06-01 13:00:48 -0700

    added controller and post route

---
+++ controllers/places-controller.js
@@ -0,0 +1,59 @@
+const { v4: uuidv4 } = require("uuid");
+const HttpError = require("../models/http-error");
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    location: {
+      lat: 40.7484474,
+      lng: -73.9871516,
+    },
+    address: "20 W 34th St, New York, NY 10001",
+    creator: "u1",
+  },
+];
+
+const getPlaceByID = (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
+  });
+
+  if (!place) {
+    throw new HttpError("Could not find a place for the provided id.", 404);
+  }
+  res.json({ place });
+};
+
+const getPlaceByUserID = (req, res, next) => {
+  const userId = req.params.uid;
+  const place = DUMMY_PLACES.find((u) => {
+    return u.creator === userId;
+  });
+  if (!place) {
+    return next(
+      new HttpError("Could not find a place for the provided user id.", 404)
+    );
+  }
+  res.json({ place });
+};
+
+const createPlace = (req, res, next) => {
+  const { title, description, coordinates, address, creator } = req.body;
+  const createdPlace = {
+    id: uuidv4(),
+    title,
+    description,
+    location: coordinates,
+    address,
+    creator,
+  };
+  DUMMY_PLACES.push(createdPlace);
+  res.status(201).json({ place: createdPlace });
+};
+
+exports.getPlaceByID = getPlaceByID;
+exports.getPlaceByUserID = getPlaceByUserID;
+exports.createPlace = createPlace;
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,45 +1,11 @@
 const express = require("express");
-const HttpError = require("../models/http-error");
-
+const controller = require("../controllers/places-controller");
 const router = express.Router();
 
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world!",
-    location: {
-      lat: 40.7484474,
-      lng: -73.9871516,
-    },
-    address: "20 W 34th St, New York, NY 10001",
-    creator: "u1",
-  },
-];
+router.get("/:pid", controller.getPlaceByID);
 
-router.get("/:pid", (req, res, next) => {
-  const placeId = req.params.pid;
-  const place = DUMMY_PLACES.find((p) => {
-    return p.id === placeId;
-  });
+router.get("/user/:uid", controller.getPlaceByUserID);
 
-  if (!place) {
-    throw new HttpError("Could not find a place for the provided id.", 404);
-  }
-  res.json({ place });
-});
-
-router.get("/user/:uid", (req, res, next) => {
-  const userId = req.params.uid;
-  const place = DUMMY_PLACES.find((u) => {
-    return u.creator === userId;
-  });
-  if (!place) {
-    return next(
-      new HttpError("Could not find a place for the provided user id.", 404)
-    );
-  }
-  res.json({ place });
-});
+router.post("/", controller.createPlace);
 
 module.exports = router;
